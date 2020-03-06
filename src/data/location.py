34a66a0f5435b5e34e190dc6a78fddb6e7c68ab5
euihyun <luh0907@gmail.com>
2020-03-06 18:46:13 +0900

    Specs revisited

--- src/data/location.py
+++ src/data/location.py
@@ -2,8 +2,10 @@ from typing import Tuple
 
 class Location:
     def __init__(self,
+                 loc_id: int,
                  name: str):
-
+        
+        self.id = loc_id
         self.name = name
         self.people = []
 
@@ -12,6 +14,7 @@ class City(Location):
     SIZE = [0, 1, 2, 3] # small, medium, big, huge
     CITY_TYPE = [0] # TBD 
     def __init__(self,
+                 city_id: int,
                  name: str,
                  size: int,
                  pos: Tuple[int, int],
@@ -20,7 +23,7 @@ class City(Location):
         assert size in self.SIZE
         assert city_type in self.CITY_TYPE
 
-        Location.__init__(self, name)
+        Location.__init__(self, city_id, name)
         self.size = size
         self.pos = pos
         self.x, self.y = pos
@@ -31,12 +34,13 @@ class City(Location):
 
 class Road(Location):
     def __init__(self,
+                 road_id: int,
                  city_from: City,
                  city_to: City,
                  road_type: int = 0,
                  name: str = None):
 
-        Location.__init__(self, name)
+        Location.__init__(self, road_id, name)
         self.city_from = city_from
         self.city_to = city_to
         self.road_type = road_type
--- src/data/world.py
+++ src/data/world.py
@@ -2,59 +2,49 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from typing import List
 
-from .location import City
+from .location import City, Road
 from .value import Color
 
 class World(nx.Graph):
     def __init__(self):
         super().__init__()
+        self.city_list = []
+        self.road_list = []
 
     def add_city(self, city: City):
+        self.city_list.append(city)
         self.add_node(city)
-
-    def add_cities_from_list(self, city_list: List[City]):
-        self.add_nodes_from(city_list)
 
     def add_cities_from_csv(self, filename: str):
         with open(filename, 'r') as f:
             for line in f:
-                name, size, x, y, city_type = line.strip().split(',')
-                city = City(name, int(size), (int(x), int(y)), int(city_type))
+                if line.startswith('#'):
+                    continue
+                city_id, name, size, x, y, city_type = line.strip().split(',')
+                city = City(city_id, name, int(size), (int(x), int(y)), int(city_type))
                 self.add_city(city)
+        self.city_list.sort(key=lambda x: x.id)
 
-    def add_road(self, city_from: City, city_to: City):
-        dist = ((city_from.x - city_to.x)**2 + (city_from.y - city_to.y)**2)**.5
-        self.add_edge(city_from, city_to, weight=dist)
-
-    def add_roads_from_list(self, city_from_to_list: List[City]):
-        for city_from, city_to in city_from_to_list:
-            self.add_road(city_from, city_to)
+    def add_road(self, road: Road):
+        self.road_list.append(road)
+        self.add_edge(road.city_from, road.city_to, weight=road.distance)
 
     def add_roads_from_csv(self, filename: str):
         with open(filename, 'r') as f:
             for line in f:
-                city_from_name, city_to_name = line.strip().split(',')
-                city_from = self.get_city_by_name(city_from_name)
-                city_to = self.get_city_by_name(city_to_name)
-                self.add_road(city_from, city_to)
-
-    def get_city_list(self) -> list:
-        return list(self.nodes)
-
-    def get_city_by_name(self, name: str) -> City:
-        city_list = self.get_city_list()
-        for city in city_list:
-            if city.name == name:
-                return city
-
-        # Not found
-        return None
+                if line.startswith('#'):
+                    continue
+                road_id, city_from, city_to = map(int, line.strip().split(','))
+                city_from = self.city_list[city_from]
+                city_to = self.city_list[city_to]
+                road = Road(road_id, city_from, city_to)
+                self.add_road(road)
+        self.road_list.sort(key=lambda x: x.id)
 
     def draw_world(self):
-        city_list = self.get_city_list()
-        city_pos_list = {city: city.pos for city in city_list}
-        city_size_list = [(city.size+1)*100 for city in city_list]
-        city_color_list = [city.owner.force.color if not city.owner is None else Color.WHITE for city in city_list]
+        city_pos_list = {city: city.pos for city in self.city_list}
+        city_size_list = [(city.size+1)*100 for city in self.city_list]
+        city_color_list = [city.owner.force.color if not city.owner is None else Color.WHITE for city in self.city_list]
         nx.draw_networkx_edges(self, city_pos_list)
         nx.draw_networkx_nodes(self, city_pos_list, node_size=city_size_list, node_color=city_color_list)
         plt.show()
